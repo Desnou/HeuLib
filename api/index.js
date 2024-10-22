949aa4c4300b49564c05a283fcb088049e859a48
Desnou <bastiangsa@gmail.com>
2024-10-21 21:09:05 -0300

    Conexión mongodb

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config(); 
 
 mongoose
-.connect(process.env.MONGO)
+.connect(process.env.MONGO)  //conexión base datos
 .then (() => {
     console.log('Connected to MongoDB');
     }).catch((err) => {
