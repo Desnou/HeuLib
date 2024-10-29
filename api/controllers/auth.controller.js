f7f0a3334f3bc5bcdfcef8345947003a3c5f3aa8
Desnou <bastiangsa@gmail.com>
2024-10-28 22:39:10 -0300

    Se crea inicio de sesión en ruta api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
@@ -13,5 +14,27 @@ export const signup = async (req, res, next) => {
     } catch (error) {
         next(error);
         
+    }
+};
+
+export const singin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        //Se valida que el usuario exista en la base de datos
+        const validUser = await User.findOne({ email });
+        //Si el usuario no existe se envía un error
+        if (!validUser) return next(errorHandler(404, "User not found!"));
+        //Se valida que la contraseña sea correcta
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        //Si la contraseña no es correcta se envía un error
+        if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
     }
 };
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -2,73 +2,83 @@ import React from 'react'
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'
 
+// Componente principal de registro de usuario
 export default function SignUp() {
+    // Estado para almacenar los datos del formulario
     const [formData, setFormData] = useState({});
+    // Estado para manejar errores
     const [error, setError] = useState(null);
+    // Estado para manejar el estado de carga
     const [loading, setLoading] = useState(false);
+    // Hook para la navegación programática
     const navigate = useNavigate();
+
+    // Función para manejar los cambios en los campos del formulario
     const handleChange = (e) => {
       setFormData({
         ...formData,
         [e.target.id]: e.target.value,
-        });
-  };
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      
-      setLoading(true);
-      const res = await fetch('/api/auth/signup', 
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(formData),
+      });
+    };
+
+    // Función para manejar el envío del formulario
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        // Establecer el estado de carga a verdadero
+        setLoading(true);
+        // Enviar una solicitud POST a la API de registro
+        const res = await fetch('/api/auth/signup', 
+          {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          }
+        );
+        // Obtener la respuesta de la API
+        const data = await res.json();
+        console.log(data);
+        // Manejar el caso en que la respuesta indique un error
+        if(data.success === false){
+          setLoading(false);
+          setError(data.message);
+          return;
         }
-      );
-      const data = await res.json();
-      console.log(data);
-      if(data.success === false){
         setLoading(false);
-        setError(data.message);
-        return;
+        setError(null);
+        navigate('/sing-in');
+
+      } catch (error) {
+        setLoading(false);
+        setError(error.message);
       }
-      setLoading(false);
-      setError(null);
-      navigate('/sing-in');
-
-    } catch (error) {
-      setLoading(false);
-      setError(error.message);
-    }
-  };
-  return (
-    <div className='p-3 max-w-lg mx-auto'>
-      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
-      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
-        {/* username */}
-        <input type="text" placeholder='username' 
-        className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
-        {/* email */}
-        <input type="email" placeholder='email' 
-        className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        {/* password */}
-        <input type="password" placeholder='password' 
-        className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
-        {/* Botones */}
-        <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
-          {loading ? 'Loading...' : 'Sign up'}</button>
-
-
-      </form>
-      <div className='flex gap-2 mt-5'>
-        <p>Ya tienes una cuenta?</p>
-        <Link to = {"/sing-in"}>
-        <span className='text-blue-700'>Sign In</span>
-        </Link>
+    };
+    return (
+      <div className='p-3 max-w-lg mx-auto'>
+        <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+          {/* username */}
+          <input type="text" placeholder='username' 
+          className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
+          {/* email */}
+          <input type="email" placeholder='email' 
+          className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+          {/* password */}
+          <input type="password" placeholder='password' 
+          className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+          {/* Botones */}
+          <button disabled={loading} className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+            {loading ? 'Loading...' : 'Sign up'}</button>
+        </form>
+        <div className='flex gap-2 mt-5'>
+          <p>Ya tienes una cuenta?</p>
+          <Link to = {"/sing-in"}>
+          <span className='text-blue-700'>Sign In</span>
+          </Link>
+        </div>
+        {error && <p className='text-red-500 text-center'>{error}</p>}
       </div>
-      {error && <p className='text-red-500 text-center'>{error}</p>}
-    </div>
-  )
+    )
 }
