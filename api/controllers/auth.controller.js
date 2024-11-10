383a27b2ae5ddd9a8c0ab1c64b9f0b5eaabf75fc
Desnou <bastiangsa@gmail.com>
2024-11-10 18:06:20 -0300

    Se agrega funcionalidad de admin al usuario

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,7 +7,14 @@ export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     // Validar que todos los campos estén presentes y no estén vacíos
-    if (!username || !email || !password || username.trim() === '' || email.trim() === '' || password.trim() === '') {
+    if (
+        !username ||
+        !email ||
+        !password ||
+        username.trim() === "" ||
+        email.trim() === "" ||
+        password.trim() === ""
+    ) {
         next(errorHandler(400, "All fields are required!"));
     }
 
@@ -25,7 +32,7 @@ export const signup = async (req, res, next) => {
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
 
-    if (!email || !password || email.trim() === '' || password.trim() === ''){
+    if (!email || !password || email.trim() === "" || password.trim() === "") {
         return next(errorHandler(400, "Todos los campos son obligatorios"));
     }
     try {
@@ -36,17 +43,22 @@ export const signin = async (req, res, next) => {
             return next(errorHandler(404, "Las credenciales son incorrectas!"));
         }
         //Se valida que la contraseña sea correcta
-        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        const validPassword = bcryptjs.compareSync(
+            password,
+            validUser.password
+        );
         //Si la contraseña no es correcta se envía un error
         if (!validPassword) {
-            return next(errorHandler(400, "Las credenciales son incorrectas!!"));
+            return next(
+                errorHandler(400, "Las credenciales son incorrectas!!")
+            );
         }
         const token = jwt.sign(
-            { id: validUser._id },
-            process.env.JWT_SECRET);
+            { id: validUser._id, isAdmin: validUser.isAdmin },
+            process.env.JWT_SECRET
+        );
         const { password: pass, ...rest } = validUser._doc;
-        res
-            .cookie("access_token", token, { httpOnly: true })
+        res.cookie("access_token", token, { httpOnly: true })
             .status(200)
             .json(rest);
     } catch (error) {
@@ -56,36 +68,46 @@ export const signin = async (req, res, next) => {
 
 /*
  * Controlador para la autenticación de usuarios a través de Google.
- * 
+ *
  * @param {Object} req - Objeto de solicitud HTTP.
  * @param {Object} res - Objeto de respuesta HTTP.
  * @param {Function} next - Función para pasar al siguiente middleware.
- * 
- * @description Este controlador maneja la autenticación de usuarios utilizando Google. 
+ *
+ * @description Este controlador maneja la autenticación de usuarios utilizando Google.
  * Si el usuario ya existe en la base de datos, se genera un token JWT y se envía en una cookie.
  * Si el usuario no existe, se crea un nuevo usuario con una contraseña generada aleatoriamente,
  * se guarda en la base de datos, se genera un token JWT y se envía en una cookie.
- * 
+ *
  * @throws {Error} Si ocurre un error durante el proceso de autenticación.
  */
 export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email: req.body.email });
         if (user) {
-            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: user._id, isAdmin:  user.isAdmin}, process.env.JWT_SECRET);
             const { password: pass, ...rest } = user._doc;
-            res
-                .cookie("access_token", token, { httpOnly: true })
+            res.cookie("access_token", token, { httpOnly: true })
                 .status(200)
                 .json(rest);
         } else {
-            const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const generatedPassword =
+                Math.random().toString(36).slice(-8) +
+                Math.random().toString(36).slice(-8);
             const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
-            const newUser = new User({ username: req.body.name.split(" ").join("").toLowerCase() + Math.random().toString(36).slice(-4), email: req.body.email, password: hashedPassword, avatar: req.body.photo });
+            const newUser = new User({
+                username:
+                    req.body.name.split(" ").join("").toLowerCase() +
+                    Math.random().toString(36).slice(-4),
+                email: req.body.email,
+                password: hashedPassword,
+                avatar: req.body.photo,
+            });
             await newUser.save();
-            const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
+            const token = jwt.sign({ id: newUser._id, isAdmin: newUser.isAdmin }, process.env.JWT_SECRET);
             const { password: pass, ...rest } = newUser._doc;
-            res.cookie("access_token", token, { httpOnly: true }).status(200).json(rest);
+            res.cookie("access_token", token, { httpOnly: true })
+                .status(200)
+                .json(rest);
         }
     } catch (error) {
         next(error);
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -22,6 +22,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "https://i.pinimg.com/736x/2f/c9/20/2fc92053c09bd431c041d91e574be860.jpg",
     },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
 }, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
