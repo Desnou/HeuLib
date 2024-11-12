af6050a6b00eb63022a02a548d5745fc6662ef49
Desnou <bastiangsa@gmail.com>
2024-11-12 00:31:49 -0300

    Se añade funcionalidad para obtener publicaciones, incluyendo paginación y filtrado. Se actualiza la interfaz de DashPosts para mostrar las publicaciones del usuario.

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,7 +3,9 @@ import { errorHandler } from "../utils/error.js";
 
 export const create = async (req, res, next) => {
     if (!req.user.isAdmin) {
-        return next(errorHandler(403, "No tienes permisos para crear una publicación"));
+        return next(
+            errorHandler(403, "No tienes permisos para crear una publicación")
+        );
     }
     if (
         !req.body.title ||
@@ -29,6 +31,58 @@ export const create = async (req, res, next) => {
         const savedPost = await newPost.save();
         res.status(201).json(savedPost);
     } catch (error) {
-        next (error);
+        next(error);
+    }
+};
+
+export const getposts = async (req, res, next) => {
+    try {
+        const startIndex = parseInt(req.query.startIndex) || 0;
+        const limit = parseInt(req.query.limit) || 10;
+        const sortDirection = req.query.order === "asc" ? 1 : -1;
+        const posts = await Post.find({
+            ...(req.query.userId && { userId: req.query.userId }),
+            ...(req.query.category && { category: { $regex: req.query.category, $options: "i" } }),
+            ...(req.query.slug && { slug: req.query.slug }),
+            ...(req.query.postId && { _id: req.query.postId }),
+            ...(req.query.author && { author: req.query.author }),
+            ...(req.query.searchTerm && {
+                $or: [
+                    { title: { $regex: req.query.searchTerm, $options: "i" } },
+                    {
+                        content: {
+                            $regex: req.query.searchTerm,
+                            $options: "i",
+                        },
+                    },
+                    {
+                        category: {
+                            $regex: req.query.searchTerm,
+                            $options: "i",
+                        },
+                    },
+                ],
+            }),
+        })
+            .sort({ updatedAt: sortDirection })
+            .skip(startIndex)
+            .limit(limit);
+
+        const totalPosts = await Post.countDocuments();
+
+        const now = new Date();
+
+        const oneMonthAgo = new Date(
+            now.getFullYear(),
+            now.getMonth() - 1,
+            now.getDate()
+        );
+
+        const lastMonthPosts = await Post.countDocuments({
+            createdAt: { $gte: oneMonthAgo },
+        });
+        res.status(200).json({ posts, totalPosts, lastMonthPosts });
+    } catch (error) {
+        next(error);
     }
 };
