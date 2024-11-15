e1cc9b2f8594c4915283b7af40641b9a83fb868b
Desnou <bastiangsa@gmail.com>
2024-11-15 06:41:46 -0300

    Se agrega la funcionalidad para mostrar artículos recientes en la página de publicación y se crea el componente PostCard para su visualización.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,39 +1,39 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp';
-import About from './pages/About';
-import CustomHeader from './components/CustomHeader';
-import PrivateRoute from './components/PrivateRoute';
-import Dashboard from './pages/Dashboard';
-import SignIn from './pages/SignIn';
-import PrivateRouteAdmin from './components/PrivateRouteAdmin';
-import CreatePost from './pages/CreatePost';
-import UpdatePost from './pages/UpdatePost';
-import PostPage from './pages/PostPage';
-import ScrollToTop from './components/ScrollToTop';
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import CustomHeader from "./components/CustomHeader";
+import PrivateRoute from "./components/PrivateRoute";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import PrivateRouteAdmin from "./components/PrivateRouteAdmin";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTop";
 
 export default function App() {
-  return <BrowserRouter>
-  <CustomHeader />
-  <ScrollToTop />
-    <Routes>
-      <Route path="/" element={<Home />} /> 
-      <Route path="/sign-in" element={<SignIn />} /> 
-      <Route path="/sign-up" element={<SignUp />} /> 
-      <Route path="/about" element={<About />} /> 
-      <Route element={<PrivateRoute />} >
-        <Route path="/dashboard" element={<Dashboard />} /> 
-      </Route>
-      <Route element={<PrivateRouteAdmin />} >
-        <Route path="/create-post" element={<CreatePost />} /> 
-        <Route path='/update-post/:postId' element={<UpdatePost />} />
-      </Route>
-      <Route path="/post/:postSlug" element={<PostPage />} />
-
-    </Routes>
-      
-    </BrowserRouter>;
-
+    return (
+        <BrowserRouter>
+            <ScrollToTop />
+            <CustomHeader />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/about" element={<About />} />
+                <Route element={<PrivateRoute />}>
+                    <Route path="/dashboard" element={<Dashboard />} />
+                </Route>
+                <Route element={<PrivateRouteAdmin />}>
+                    <Route path="/create-post" element={<CreatePost />} />
+                    <Route
+                        path="/update-post/:postId"
+                        element={<UpdatePost />}
+                    />
+                </Route>
+                <Route path="/post/:postSlug" element={<PostPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,13 +2,15 @@ import { Button, Spinner } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import CallToAction from "../components/CalltoAction";
-import CommentSection from '../components/CommentSection';
+import CommentSection from "../components/CommentSection";
+import PostCard from "../components/PostCard";
 
 export default function PostPage() {
     const { postSlug } = useParams();
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
     const [post, setPost] = useState(null);
+    const [recentPosts, setRecentPosts] = useState(null);
 
     useEffect(() => {
         const fetchPost = async () => {
@@ -33,6 +35,22 @@ export default function PostPage() {
         };
         fetchPost();
     }, [postSlug]);
+
+    useEffect(() => {
+        try {
+            const fetchRecentPosts = async () => {
+                const res = await fetch(`/api/post/getposts?limit=3`);
+                const data = await res.json();
+                if (res.ok) {
+                    setRecentPosts(data.posts);
+                }
+            };
+            fetchRecentPosts();
+        } catch (error) {
+            console.log(error.message);
+        }
+    }, []);
+
     if (loading)
         return (
             <div className="flex justify-center items-center min-h-screen">
@@ -42,7 +60,7 @@ export default function PostPage() {
 
     return (
         <main className="p-3 flex flex-col max-w-6xl mx-auto min-h-screen">
-            <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl ">
+            <h1 className="text-3xl mt-10 p-3 text-center font-serif max-w-2xl mx-auto lg:text-4xl">
                 {post && post.title}
             </h1>
             <div className="flex flex-wrap justify-center border-b border-slate-500">
@@ -60,10 +78,12 @@ export default function PostPage() {
                     ))}
             </div>
             <div className="flex justify-center gap-2 p-2">
-                <span className="font-semibold text-lg text-gray-700">Autor : </span>
+                <span className="font-semibold text-lg text-gray-700">
+                    Autor :{" "}
+                </span>
                 <Link
                     to={`/search?author=${post && post.author}`}
-                    className="font-bold text-lg hover:underline italic" 
+                    className="font-bold text-lg hover:underline italic"
                 >
                     {post && post.author}
                 </Link>
@@ -82,13 +102,24 @@ export default function PostPage() {
                     lectura
                 </span>
             </div>
-            <div className='p-3 max-w-2xl mx-auto w-full post-content'>
-                <div dangerouslySetInnerHTML={{__html: post && post.content}}/>
+            <div className="p-3 max-w-2xl mx-auto w-full post-content">
+                <div
+                    dangerouslySetInnerHTML={{ __html: post && post.content }}
+                />
             </div>
             <div className="max-w-4xl mx-auto w-full">
                 <CallToAction />
             </div>
-            <CommentSection postId={post._id}/>
+            <CommentSection postId={post._id} />
+            <div className="flex flex-col justify-center items-center mb-5">
+                <h1 className="text-xl mt-5">Recent articles</h1>
+                <div className="flex flex-wrap gap-5 mt-5 justify-center">
+                    {recentPosts &&
+                        recentPosts.map((post) => (
+                            <PostCard key={post._id} post={post} />
+                        ))}
+                </div>
+            </div>
         </main>
     );
 }
